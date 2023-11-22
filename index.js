6b0508acf76f12b67a5e8406fc101b8a3d18eddd
cornel2012 <mich_corneliu2000@yahoo.com>
2023-11-22 18:52:41 +0200

    Move some images in public folder
    Hope this will fix images that are not deployed on netlify.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const addItemToList = () => {
 
   if (inputValue) {
     push(shoppingListInDB, item);
-	animateHeader('/images/animated-cat.gif', 1000);
+	animateHeader('/public/animated-cat.gif', 1000);
 
     clearInputFieldEl();
   }
@@ -104,7 +104,7 @@ function appendItemToShoppingListEl(item) {
     } else {
       updates[`shoppingList/${itemID}/selected/`] = true;
     }	
-	animateHeader('/images/cart.gif');
+	animateHeader('/public/cart.gif');
     update(ref(database), updates);
   });
 
